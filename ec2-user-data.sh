699d02e8524440a65ca32ccf2bc8014f380aa14d
vishal <itsvr619@gmail.com>
2022-05-11 21:31:33 +0530

    new1

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -4,5 +4,4 @@
 yum update -y
 yum install -y httpd
 systemctl start httpd
-systemctl enable httpd
-echo "<h1>welcome back to the hello world from $(hostname -f)</h1>" > /var/www/html/index.html
+systemctl enablecho "<h1>welcome back to the hello world of new branch from $(hostname -f)</h1>" > /var/www/html/index.html
